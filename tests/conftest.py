43d25ee2eb331b9111a273ac5d134b6c1c9e1eda
giocaizzi <giocaizzi@gmail.com>
2024-06-12 20:32:36 +0200

    test passes

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,10 @@ import pandas as pd
 
 # ---- Test data ----
 
+
 @pytest.fixture
 def test_prices():
     """read csv test_prices.csv"""
-    return pd.read_csv("tests/test_prices.csv")
+    df = pd.read_csv("tests/test_prices.csv")
+    df["Date"] = pd.to_datetime(df["Date"])
+    return df
